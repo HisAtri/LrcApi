e817984d5ffa8630c7d4580aa496357f4a056547
HisAtri <yz@ghacg.com>
2023-10-22 01:05:01 +0800

    支持咪咕API接口

--- mod/api.py
+++ mod/api.py
@@ -13,7 +13,8 @@ def get_lyrics_from_net(title, artist, album):
 
     # 使用歌曲名和作者名查询歌曲
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 LrcAPI/1.1', }
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 LrcAPI/1.1',
+    }
     # 第一层Json，要求获得Hash值
     response = requests.get(
         f'http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword={searcher}&page=1&pagesize=2&showtype=1',
@@ -56,11 +57,29 @@ def api_2(title, artist, album):
         return None
 
 
+def migu(title, artist, album):
+    headers = {
+        "Referer": "http://music.migu.cn/"
+    }
+    search_str = title+artist+album
+    search_response = requests.get(f"https://pd.musicapp.migu.cn/MIGUM2.0/v1.0/content/search_all.do?&ua=Android_migu&version=5.0.1&text={search_str}&pageNo=1&pageSize=10&searchSwitch={{\"song\":1,\"album\":0,\"singer\":0,\"tagSong\":0,\"mvSong\":0,\"songlist\":0,\"bestShow\":1}}")
+    res_json = search_response.json()
+    result = res_json["songResultData"]["result"]
+    for music_detail in result:
+        if music_detail["resourceType"] == "2" & music_detail["name"] == title:
+            lyrics_url = music_detail["lyricUrl"]
+            lyrics_response = requests.get(lyrics_url, headers=headers)
+            return lyrics_response.text
+    time.sleep(10)
+    return None
+
+
 def main(title, artist, album):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # 提交各API函数到线程池中执行
         future_a = executor.submit(get_lyrics_from_net, title, artist, album)
         future_b = executor.submit(api_2, title, artist, album)
+        future_c = executor.submit(migu, title, artist, album)
         # 等待任意一个API完成
         done, not_done = concurrent.futures.wait([future_a, future_b], return_when=concurrent.futures.FIRST_COMPLETED)
         # 获取已完成线程的返回结果
