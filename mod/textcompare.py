8c6e0b1c18de701d5e757f4c98d5fbd93922b451
HisAtri <yz@ghacg.com>
2023-11-26 12:48:50 +0800

    优化文本比较逻辑

--- mod/textcompare.py
+++ mod/textcompare.py
@@ -36,14 +36,32 @@ def longest_common_substring(str1, str2):
     return max_length
 
 
+def str_duplicate_rate(str1, str2):
+    """
+    用于计算重复字符
+    """
+    set1 = set(str1)
+    set2 = set(str2)
+
+    common_characters = set1.intersection(set2)
+    total_characters = set1.union(set2)
+
+    similarity_ratio = len(common_characters) / len(total_characters)
+    return similarity_ratio
+
+
 def calculate_duplicate_rate(list_1, list_2):
     """
     用于计算重复词素
     """
     count = 0  # 计数器
     for char in list_1:
-        if char in list_2:
-            count += 1
+        char_sim = []
+
+        # 对每个词素进行association计算
+        for char_s in list_2:
+            char_sim.append(association(char, char_s))
+        count += max(char_sim)
     duplicate_rate = count / len(list_1)  # 计算重复率
     return duplicate_rate
 
@@ -53,11 +71,11 @@ def association(text_1: str, text_2: str) -> float:
     """
     通过相对最大匹配距离、相对最小编辑长度（ED）
     测量文本相似度
-    以text_1为基准
-    权重混合采用x*e^(y-1)
-    具体为什么用这个我也不知道
-    反正少量实验分离度似乎比算数平均值和几何平均值更好？
-    *无所叼胃能用就行
+    最长相似、字符重复结合
+    权重混合
+    :param text_1: 用户传入文本
+    :param text_2: 待比较文本
+    :return: 相似度 float: 0~1
     """
     if text_1 == '':
         return 0.5
@@ -66,7 +84,8 @@ def association(text_1: str, text_2: str) -> float:
     text_1 = text_1.lower()
     text_2 = text_2.lower()
     common_ratio = longest_common_substring(text_1, text_2) / len(text_1)
-    similar_ratio = common_ratio
+    string_dr = str_duplicate_rate(text_1, text_2)
+    similar_ratio = common_ratio * (string_dr ** 0.5) ** (1 / 1.5)
     return similar_ratio
 
 
@@ -94,4 +113,4 @@ def zero_item(text: str) -> str:
 if __name__ == "__main__":
     text_s = "aaaa&bbbb&ccccc"
     text_r = "aaaa&ccccc&bbbb"
-    print(assoc_artists(text_s, text_r))
+    print(str_duplicate_rate(text_s, text_r))
