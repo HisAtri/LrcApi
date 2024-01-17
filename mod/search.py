152ef3ff953e2fa94fd45a0412149c1d70e97dd1
HisAtri <yz@ghacg.com>
2024-01-17 19:45:54 +0800

    更新软件包要求版本

--- mod/search.py
+++ mod/search.py
@@ -1,8 +1,8 @@
 import base64
 import json
-
 import aiohttp
 import asyncio
+
 from mod import textcompare
 
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
