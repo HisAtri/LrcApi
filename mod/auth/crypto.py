99cb2dd44d32d6ba08578d6a6f033e0da28e0eba
HisAtri <yz@ghacg.com>
2024-02-29 01:35:25 +0800

    增加注释

--- mod/auth/crypto.py
+++ mod/auth/crypto.py
@@ -7,16 +7,30 @@ class Crypto:
 
     @staticmethod
     def gen_key():
+        """
+        生成一个32位的AES密钥
+        :return: 32位随机字符串作AES密钥
+        """
         import random
         import string
         aes_key: str = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
         return aes_key
 
     def encrypt(self, data: str) -> str:
+        """
+        加密数据，只接收JSON字符串，不负责JSON编解码
+        :param data: json string
+        :return: encrypted data
+        """
         aes = pyaes.AESModeOfOperationCTR(self.key.encode())
         return aes.encrypt(data).hex()
 
     def decrypt(self, data: str) -> str:
+        """
+        解密数据，返回JSON字符串
+        :param data: encrypted data
+        :return: json string
+        """
         aes = pyaes.AESModeOfOperationCTR(self.key.encode())
         return aes.decrypt(bytes.fromhex(data)).decode()
 
