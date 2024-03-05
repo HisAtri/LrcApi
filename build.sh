1b5d1cc2df6da3a55ad479a43d10a945da267457
HisAtri <yz@ghacg.com>
2024-03-05 21:34:41 +0800

    修改Dockerfile，增加debug运行方式

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 ### 此文件用于构建本地Docker镜像
-### 请确保工作目录位于项目根目录
 docker build -t lrcapi:local .
--- mod/args/__init__.py
+++ mod/args/__init__.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="启动LRCAPI服务器")
 # 添加一个 `--port` 参数，默认值28883
 parser.add_argument('--port', type=int, default=28883, help='应用的运行端口，默认28883')
 parser.add_argument('--auth', type=str, default='', help='用于验证Header.Authentication字段，建议纯ASCII字符')
+parser.add_argument("--debug", action="store_true", help="Enable debug mode")
 kw_args, unknown_args = parser.parse_known_args()
 arg_auths: dict = {kw_args.auth: "rwd"} if kw_args.auth else None
 
@@ -87,6 +88,7 @@ class GlobalArgs:
             self.auth: dict = {}
         self.port = first(env_args.port, kw_args.port, config_args.port, default.port)
         self.ip = first(config_args.ip, default.ip)
+        self.debug = kw_args.debug
 
     def valid(self, key):
         """
