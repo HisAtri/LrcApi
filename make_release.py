3193eb7252086f6ab4bfbce6040106186ca50932
HisAtri <yz@ghacg.com>
2024-04-01 13:59:19 +0800

    no message

--- make_release.py
+++ make_release.py
@@ -10,9 +10,9 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 container_root_dir = '/app'
 
 # 在容器内使用buildup.py脚本自动化完成依赖安装、打包等操作
-command_in_docker = "\"cd /app && python buildup.py\""
+command_in_docker = "\"python buildup.py\""
 
 for platform in platforms:
     # 使用docker run命令在不同平台的容器中执行打包命令
-    command = f"docker run --rm -v {root_dir}:{container_root_dir} --platform {platform} {base_image} {command_in_docker}"
-    subprocess.run(command, shell=True)
+    # 运行目录为容器的 /app 目录
+    command = f"docker run --rm -v {root_dir}:{container_root_dir} --platform {platform} -w {container_root_dir} {base_image} bash -c {command_in_docker}"
