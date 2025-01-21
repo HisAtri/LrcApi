abf449673be0309376f9d45ef91b0ee9f18e1cf0
hisatri <yz@ghacg.com>
2025-01-21 23:28:09 +0800

    使用Cloudflare代理的API端点

--- mod/searchx/api.py
+++ mod/searchx/api.py
@@ -3,13 +3,13 @@ import requests
 
 headers = {
     "Host": "127.0.0.1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 LrcAPI"
 }
 
 
 def search(title='', artist='', album='') -> list:
     try:
-        url = f"http://cn.lrc.cx:880/jsonapi?title={title}&artist={artist}&album={album}&path=None&limit=1&api=lrcapi"
+        url = f"https://api.lrc.cx/jsonapi?title={title}&artist={artist}&album={album}&path=None&limit=1&api=lrcapi"
         response = requests.get(url, headers=headers)
         return response.json()
     except Exception as e:
